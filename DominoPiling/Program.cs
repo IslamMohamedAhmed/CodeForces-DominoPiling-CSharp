2e6a021a9ed8f0b5fac6fa9a6b0d847ec8ab541e
unknown <islammohamedahmed49@gmail.com>
2024-04-12 22:21:00 +0200

    dp

---
+++ DominoPiling/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+
+int m, n;
+
+string str = ReadLine();
+
+var arr = str.Split(" ");
+m = ToInt32(arr[0]);
+n = ToInt32(arr[1]);
+
+WriteLine(m * n / 2);
